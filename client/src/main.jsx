a814ec98089c2954e9807605fb0b243912d3368f
Mr Naveen <116378318+MrNaveen669@users.noreply.github.com>
2026-09-16 14:42:44 +0530

    feat: wire account provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { CommerceProvider } from './state/CommerceContext.jsx';
+import { AuthProvider } from './state/AuthContext.jsx';
 import './styles.css';
 import './commerce.css';
+import './account.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CommerceProvider><App /></CommerceProvider>
+      <AuthProvider><CommerceProvider><App /></CommerceProvider></AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
