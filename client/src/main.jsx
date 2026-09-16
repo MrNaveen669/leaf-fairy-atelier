2bc1244c952c4bbb63f685037d564380c2e33b71
Mr Naveen <116378318+MrNaveen669@users.noreply.github.com>
2026-09-16 14:39:38 +0530

    feat: load commerce styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { CommerceProvider } from './state/CommerceContext.jsx';
 import './styles.css';
+import './commerce.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
