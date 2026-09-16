bfd7d5ddbb9c9bd14ac58855aff8f7322b06db4d
Mr Naveen <116378318+MrNaveen669@users.noreply.github.com>
2026-09-16 17:01:07 +0530

    phase 1.8.10: wire resilient admin runtime

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AdminApp from './AdminApp';
+import AdminRuntime from './AdminRuntime';
 import './styles.css';
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><AdminApp /></React.StrictMode>);
+createRoot(document.getElementById('root')).render(<React.StrictMode><AdminRuntime /></React.StrictMode>);
